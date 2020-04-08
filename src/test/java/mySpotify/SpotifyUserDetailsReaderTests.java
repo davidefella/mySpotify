7d2edb46b68d80dce40c82a59e863f03b6e59981
Davide Fella <davide.fella@gmail.com>
2020-04-08 03:00:28 +0200

    Add first tests

---
+++ src/test/java/mySpotify/SpotifyUserDetailsReaderTests.java
@@ -0,0 +1,27 @@
+package mySpotify;
+
+import mySpotify.controller.api.ArtistsReader;
+import mySpotify.controller.api.TracksReader;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpotifyUserDetailsReaderTests {
+
+  @Autowired
+  ArtistsReader artistsReader;
+
+  @Autowired
+  TracksReader tracksReader;
+
+  @Test
+  public void contextLoads(){
+    Assert.assertNotNull(artistsReader);
+    Assert.assertNotNull(tracksReader);
+  }
+}
---
+++ src/test/java/mySpotify/controller/ArtistsReaderTest.java
@@ -0,0 +1,18 @@
+package mySpotify.controller;
+
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArtistsReaderTest {
+
+  @Test
+  public void Artists_response_artistsRead(){
+
+  }
+
+}
