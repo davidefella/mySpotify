afe98d6afaf4df035e8e47d087c5ec875942cb89
Davide Fella <davide.fella@gmail.com>
2020-04-16 00:45:24 +0200

    Group for music genre

--- src/main/java/mySpotify/mapper/GenreMapper.java
+++ src/main/java/mySpotify/mapper/GenreMapper.java
@@ -3,6 +3,8 @@ package mySpotify.mapper;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TreeMap;
 import mySpotify.MySpotifyApplication;
 import mySpotify.model.Artists.Items;
@@ -14,7 +16,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class GenreMapper {
 
+  Logger logger = LoggerFactory.getLogger(MySpotifyApplication.class);
+
   public Map<String, Integer> countGenreFromArtists(List<Items> artists) {
+
+    Map<String, Integer> allGenreToCounting = countAllGenresFromArtists(artists);
+    Map<String, Integer> genreGrouped = groupMusicGenre(allGenreToCounting);
+
+    return genreGrouped;
+
+  }
+
+  private Map<String, Integer> countAllGenresFromArtists(List<Items> artists) {
     Map<String, Integer> genreToCounting = new HashMap<>();
     CountingGenreComparator countingGenreComparator = new CountingGenreComparator(genreToCounting);
 
@@ -36,5 +49,58 @@ public class GenreMapper {
     newMap.putAll(genreToCounting);
 
     return newMap;
+
   }
+
+  private Map<String, Integer> groupMusicGenre(Map<String, Integer> allGenreToCounting) {
+    Map<String, Integer> countMusicGenres = new HashMap<>();
+    String superGenre;
+
+    Set<Entry<String, Integer>> setFromTreeMaps = allGenreToCounting.entrySet();
+    for (Map.Entry<String, Integer> entry : setFromTreeMaps) {
+
+      superGenre = getGenreGrouped(entry.getKey());
+
+      if (countMusicGenres.containsKey(superGenre)) {
+        countMusicGenres
+            .put(superGenre, (countMusicGenres.get(superGenre) + entry.getValue()));
+      } else {
+        countMusicGenres.put(superGenre, entry.getValue());
+      }
+    }
+
+    return countMusicGenres;
+  }
+
+  private String getGenreGrouped(String genre) {
+    String superGenre = "other";
+
+    if (genre.contains("rock")) {
+      return "rock";
+    } else if (genre.contains("metal") || genre.equals("neue deutsche harte") || genre
+        .equals("wrestling") || genre.equals("old school thrash") || genre.equals("industrial")
+        || genre.equals("slayer")) {
+      return "metal";
+    } else if (genre.contains("grunge")) {
+      return "grunge";
+    } else if (genre.contains("hip hop")) {
+      return "hip pop";
+    } else if (genre.contains("pop") || genre.equals("italian alternative") || genre
+        .equals("italian reggae")) {
+      return "pop";
+    } else if (genre.contains("punk") || genre.equals("chicago hardcore") || genre
+        .equals("permanent wave")) {
+      return "punk";
+    } else if (genre.contains("indie")) {
+      return "indie";
+    } else if (genre.contains("rap")) {
+      return "rap";
+    }
+
+    logger.warn(genre);
+
+    return superGenre;
+  }
+
+
 }
