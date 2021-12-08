787d331cff1baaf64168b3ca6505a7457d128823
Davide <davidefella@users.noreply.github.com>
2021-12-08 17:55:55 +0100

    Refactoring

--- src/main/java/mySpotify/util/TokenGenerator.java
+++ src/main/java/mySpotify/util/TokenGenerator.java
@@ -3,6 +3,7 @@ package mySpotify.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -15,49 +16,49 @@ import mySpotify.model.Spotify.SpotifyAuthResponse;
 @Service
 public final class TokenGenerator {
 
-  SpotifyAuthResponse spotifyAuthResponse;
+	SpotifyAuthResponse spotifyAuthResponse;
 
-  @Autowired
-  MySpotifyUtils mySpotifyUtils;
+	@Autowired
+	MySpotifyUtils mySpotifyUtils;
 
-  @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
-  public String getBearerToken() {
+	@Value("${refresh_token}")
+	private String refreshToken;
 
-    String refreshToken = mySpotifyUtils.readRefreshToken();
+	@PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+	public String getBearerToken() {
 
-    spotifyAuthResponse = this
-        .getNewBearerTokenFromRefreshToken(refreshToken);
+		spotifyAuthResponse = this.getNewBearerTokenFromRefreshToken(refreshToken);
 
-    return spotifyAuthResponse.getAccess_token();
-  }
+		return spotifyAuthResponse.getAccess_token();
+	}
 
-  private SpotifyAuthResponse getNewBearerTokenFromRefreshToken(String refresh_token) {
-    SpotifyAuthResponse spotifyAuthResponse = new SpotifyAuthResponse();
+	private SpotifyAuthResponse getNewBearerTokenFromRefreshToken(String refresh_token) {
+		SpotifyAuthResponse spotifyAuthResponse = new SpotifyAuthResponse();
 
-    final String GRANT_TYPE = "refresh_token";
-    String authorizationCode = "NjA1ZTgxNzIzMDRmNDM0MWFlNGEzMWI4MmFjMzdkYjI6NmMxYjBmOTA5YzNmNGQwNzk3Y2YyNjU0NTdhZmVhMmU=";
+		final String GRANT_TYPE = "refresh_token";
+		String authorizationCode = "NjA1ZTgxNzIzMDRmNDM0MWFlNGEzMWI4MmFjMzdkYjI6NmMxYjBmOTA5YzNmNGQwNzk3Y2YyNjU0NTdhZmVhMmU=";
 
-    HttpHeaders headers = new HttpHeaders();
-    headers.set("Authorization", "Basic " + authorizationCode);
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Basic " + authorizationCode);
 
-    LinkedMultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
-    paramsMap.add("grant_type", GRANT_TYPE);
-    paramsMap.add("refresh_token", refresh_token);
+		LinkedMultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
+		paramsMap.add("grant_type", GRANT_TYPE);
+		paramsMap.add("refresh_token", refresh_token);
 
-    HttpEntity<LinkedMultiValueMap<String, String>> request = new HttpEntity<>(paramsMap, headers);
+		HttpEntity<LinkedMultiValueMap<String, String>> request = new HttpEntity<>(paramsMap, headers);
 
-    RestTemplate restTemplate = new RestTemplate();
-    ResponseEntity<String> result = restTemplate
-        .postForEntity(mySpotifyUtils.readAuthorizationEndpoints(), request, String.class);
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> result = restTemplate.postForEntity(mySpotifyUtils.readAuthorizationEndpoints(), request,
+				String.class);
 
-    ObjectMapper mapper = new ObjectMapper();
+		ObjectMapper mapper = new ObjectMapper();
 
-    try {
-      spotifyAuthResponse = mapper.readValue(result.getBody(), SpotifyAuthResponse.class);
-    } catch (JsonProcessingException e) {
-      e.printStackTrace();
-    }
+		try {
+			spotifyAuthResponse = mapper.readValue(result.getBody(), SpotifyAuthResponse.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
 
-    return spotifyAuthResponse;
-  }
+		return spotifyAuthResponse;
+	}
 }
